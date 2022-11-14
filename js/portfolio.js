f36cf96a5bbe325bc1e07e8bb315806c37611129
Peter Emhofer <p.emhofer@gmail.com>
2022-11-14 20:23:15 +0100

    expanded stack, added portfolio from github

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,18 +1,35 @@
 import { request } from "https://cdn.skypack.dev/@octokit/request";
 import githubToken from "../_ignore/config.js";
 
-const result = await request("GET /repos/{owner}/{repo}", {
+const result = await request("GET /users/{owner}/repos", {
   headers: {
     authorization: `token ${githubToken}`,
   },
   owner: "emhofer",
-  repo: "Birb",
 });
 
-const cardTest = document.getElementById("cardTest");
-cardTest.firstElementChild.innerHTML = result.data.name;
-cardTest.children.item(1).innerHTML = result.data.description;
-cardTest.children.item(2).innerHTML = result.data.language;
+const containerCards = document.getElementById("containerCards");
 
-console.log(result.data.name);
-console.log(cardTest.firstElementChild);
+console.log(result.data[1]);
+
+for (let i = 0; i < result.data.length; i++) {
+  const repo = result.data[i];
+  const repoCard = document.createElement("div");
+  repoCard.classList.add("card");
+  const repoTitle = document.createElement("h1");
+  const repoDescription = document.createElement("p");
+  const repoLanguage = document.createElement("div");
+  repoLanguage.classList.add("githubInfo");
+  repoTitle.innerHTML = repo.name;
+  repoDescription.innerHTML = repo.description;
+  repoLanguage.innerHTML = repo.language;
+  repoCard.appendChild(repoTitle);
+  repoCard.appendChild(repoDescription);
+  repoCard.appendChild(repoLanguage);
+  containerCards.appendChild(repoCard);
+}
+
+// const cardTest = document.getElementById("cardTest");
+// cardTest.firstElementChild.innerHTML = result.data.name;
+// cardTest.children.item(1).innerHTML = result.data.description;
+// cardTest.children.item(2).innerHTML = result.data.language;
